8a73048d600706342dec23d28ac624d439dc06a6
Koji Wakamiya <koji.wakamiya@gmail.com>
2024-02-11 00:38:19 +0900

    chore: Cleanup

--- lib/static_map_service.dart
+++ lib/static_map_service.dart
@@ -1,8 +1,1 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
-
-export 'src/static_map_service_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+library static_map_service;
