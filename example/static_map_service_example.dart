c1016c0edd44014344ef0ec294fd30c33a25c98b
Koji Wakamiya <koji.wakamiya@gmail.com>
2024-02-10 21:41:16 +0900

    Create project

---
+++ example/static_map_service_example.dart
@@ -0,0 +1,6 @@
+import 'package:static_map_service/static_map_service.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/static_map_service.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/static_map_service_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
