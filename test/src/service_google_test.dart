b3b131f6a893e999b7a1ad657c8c7fecfe698ef1
Koji Wakamiya <koji.wakamiya@gmail.com>
2024-02-14 09:47:03 +0900

    chore: Add test

--- test/src/service_google_test.dart
+++ test/src/service_google_test.dart
@@ -3,22 +3,22 @@ import 'package:static_map_service/src/shared.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of center', () {
-    const key = 'static_map_service_key';
-    const zoom = 10;
-    const mapSize = GoogleMapSize(
-      width: 400,
-      height: 400,
-    );
-    const tokyoStation = MapLatLng(
-      latitude: 35.6812,
-      longitude: 139.7671,
-    );
+  const key = 'static_map_service_key';
+  const zoom = 10;
+  const mapSize = GoogleMapSize(
+    width: 400,
+    height: 400,
+  );
+  const tokyoStationLatLng = MapLatLng(
+    latitude: 35.6812,
+    longitude: 139.7671,
+  );
 
+  group('A group of center', () {
     test('required parameters', () {
       const service = GoogleMapService.center(
         key: key,
-        center: tokyoStation,
+        center: tokyoStationLatLng,
         zoom: zoom,
         size: mapSize,
       );
@@ -29,10 +29,10 @@ void main() {
       );
     });
 
-    test('input_url', () {
+    test('path_and_params', () {
       const service = GoogleMapService.center(
         key: key,
-        center: tokyoStation,
+        center: tokyoStationLatLng,
         zoom: zoom,
         size: mapSize,
       );
@@ -46,7 +46,7 @@ void main() {
     test('signature', () {
       final service = GoogleMapService.center(
         key: key,
-        center: tokyoStation,
+        center: tokyoStationLatLng,
         zoom: zoom,
         size: mapSize,
         signatureFunction: (inputUrl) => 'signature',
@@ -55,6 +55,157 @@ void main() {
       expect(
         service.url,
         'https://maps.googleapis.com/maps/api/staticmap?center=35.6812%2C139.7671&zoom=10&size=400x400&key=static_map_service_key&signature=signature',
+      );
+    });
+
+    test('scale (1)', () {
+      final service = GoogleMapService.center(
+        key: key,
+        center: tokyoStationLatLng,
+        zoom: zoom,
+        size: mapSize,
+        scale: 1,
+      );
+
+      expect(
+        service.url,
+        'https://maps.googleapis.com/maps/api/staticmap?center=35.6812%2C139.7671&zoom=10&size=400x400&key=static_map_service_key',
+      );
+    });
+
+    test('scale (2)', () {
+      final service = GoogleMapService.center(
+        key: key,
+        center: tokyoStationLatLng,
+        zoom: zoom,
+        size: mapSize,
+        scale: 2,
+      );
+
+      expect(
+        service.url,
+        'https://maps.googleapis.com/maps/api/staticmap?center=35.6812%2C139.7671&zoom=10&size=400x400&scale=2&key=static_map_service_key',
+      );
+    });
+
+    test('format (png)', () {
+      final service = GoogleMapService.center(
+        key: key,
+        center: tokyoStationLatLng,
+        zoom: zoom,
+        size: mapSize,
+        format: GoogleMapFormat.png,
+      );
+
+      expect(
+        service.url,
+        'https://maps.googleapis.com/maps/api/staticmap?center=35.6812%2C139.7671&zoom=10&size=400x400&key=static_map_service_key',
+      );
+    });
+
+    test('format (jpg)', () {
+      final service = GoogleMapService.center(
+        key: key,
+        center: tokyoStationLatLng,
+        zoom: zoom,
+        size: mapSize,
+        format: GoogleMapFormat.jpg,
+      );
+
+      expect(
+        service.url,
+        'https://maps.googleapis.com/maps/api/staticmap?center=35.6812%2C139.7671&zoom=10&size=400x400&format=jpg&key=static_map_service_key',
+      );
+    });
+  });
+
+  final tokyoStationMarker = GoogleMapMarkers(
+    locations: {
+      tokyoStationLatLng,
+    },
+  );
+
+  group('A group of markers', () {
+    test('required parameters', () {
+      final service = GoogleMapService.markers(
+        key: key,
+        markers: {
+          tokyoStationMarker,
+        },
+        zoom: zoom,
+        size: mapSize,
+      );
+
+      expect(
+        service.url,
+        'https://maps.googleapis.com/maps/api/staticmap?zoom=10&size=400x400&markers=35.6812%2C139.7671&key=static_map_service_key',
+      );
+    });
+
+    test('path_and_params', () {
+      final service = GoogleMapService.markers(
+        key: key,
+        markers: {
+          tokyoStationMarker,
+        },
+        zoom: zoom,
+        size: mapSize,
+      );
+
+      expect(
+        service.pathAndParams,
+        '/maps/api/staticmap?zoom=10&size=400x400&markers=35.6812%2C139.7671&key=static_map_service_key',
+      );
+    });
+
+    test('signature', () {
+      final service = GoogleMapService.markers(
+        key: key,
+        markers: {
+          tokyoStationMarker,
+        },
+        zoom: zoom,
+        size: mapSize,
+        signatureFunction: (inputUrl) => 'signature',
+      );
+
+      expect(
+        service.url,
+        'https://maps.googleapis.com/maps/api/staticmap?zoom=10&size=400x400&markers=35.6812%2C139.7671&key=static_map_service_key&signature=signature',
+      );
+    });
+
+    test('scale (1)', () {
+      final service = GoogleMapService.markers(
+        key: key,
+        markers: {
+          tokyoStationMarker,
+        },
+        zoom: zoom,
+        size: mapSize,
+        scale: 1,
+      );
+
+      expect(
+        service.url,
+        'https://maps.googleapis.com/maps/api/staticmap?zoom=10&size=400x400&markers=35.6812%2C139.7671&key=static_map_service_key',
+      );
+    });
+
+    test('scale (2)', () {
+      final service = GoogleMapService.markers(
+        key: key,
+        markers: {
+          tokyoStationMarker,
+        },
+        zoom: zoom,
+        size: mapSize,
+        scale: 2,
+      );
+
+      expect(
+        service.url,
+        'https://maps.googleapis.com/maps/api/staticmap?zoom=10&size=400x400&scale=2&markers=35.6812%2C139.7671&key=static_map_service_key',
       );
     });
   });
